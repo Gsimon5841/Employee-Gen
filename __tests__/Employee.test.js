af0dc51bb1e55854accf4ba05379b7af88af282b
Remnant <georgesimon4801@gmail.com>
2022-05-14 16:42:03 -0400

    project init

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,10 @@
+const Employee = require("../lib/Employee.js");
+
+test("creates an employee object", () => {
+    const employee = new Employee("Danny", "Danny@fenton.com", 1);
+
+    expect(employee.getName()).toBe("Danny");
+    expect(employee.getEmail()).toBe("Danny@fenton.com");
+    expect(employee.getId()).toBe(1);
+    expect(employee.getRole()).toBe("Employee");
+});
